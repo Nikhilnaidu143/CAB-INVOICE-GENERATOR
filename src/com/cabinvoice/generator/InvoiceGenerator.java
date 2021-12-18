7ab8e28c533b726fe9c016a7a46cf982b00c4e54
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-18 14:46:42 +0530

    START:- Displayed Welcome Message.

---
+++ src/com/cabinvoice/generator/InvoiceGenerator.java
@@ -0,0 +1,7 @@
+package com.cabinvoice.generator;
+
+public class InvoiceGenerator {
+	public static void main(String[] args) {
+		System.out.println("--------WELCOME TO CAB INVOICE GENERATOR--------");
+	}
+}
