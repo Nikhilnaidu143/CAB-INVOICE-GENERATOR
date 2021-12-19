fa5fa77081fabd4150ba2dfa4bf476e64e403f60
saitarun135 <nikhilnaidu473@gmail.com>
2021-12-19 14:11:26 +0530

    STEP-1:- Calculated Fare.

--- src/com/cabinvoice/generator/InvoiceGenerator.java
+++ src/com/cabinvoice/generator/InvoiceGenerator.java
@@ -5,9 +5,11 @@ public class InvoiceGenerator {
 	/** Constant variables. **/
 	public static final double COST_PER_KILOMETER = 10.0;
 	public static final int COST_PER_MINUTE = 1;
+	public static final int MINIMUM_FARE = 5;
 
 	/** Calculating fare method. **/
 	public double calculateFare(double distance, int time) {
-		return distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
+		double totalFare = distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
+		return Math.max(totalFare, MINIMUM_FARE);
 	}
 }
